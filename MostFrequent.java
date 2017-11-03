a53151fd39b9c0eaee377cf6d779a271a6720ba6
nellery <31356235+nellery@users.noreply.github.com>
2017-11-03 12:26:09 -0700

    Update MostFrequent.java

--- MostFrequent.java
+++ MostFrequent.java
@@ -1,32 +1,31 @@
 public class MostFrequent {
     public static void main (String args[]) {
-        findMostFreq("0011");
+        findMostFreq("3,4,2,6,6,6,2,0,2");
     }
 
     public static void findMostFreq(String str) {
         int count=0;
-        char mostFrequent='h';
+        char mostFrequent='0';
         int max=0;
-        
+
+        str=str.replace(",","");
         for(int i=0; i<str.length();i++) {
             count=0;
             for (int j=i+1;j<str.length();j++) {
-                if (j+count<str.length() && 
-                    i+count<str.length() &&
-                    str.charAt(j)==str.charAt(i)) {
-                    count++;
-                }
-                if (count>max) {
-                    max=count;
-                    mostFrequent=str.charAt(j);
-                }
-                else if (count==max) {
-                    if(str.charAt(j)>Integer.parseInt(Character.toString(mostFrequent))) {
-                        mostFrequent=str.charAt(j);
-                    }
-                }
+                if (str.charAt(j)==str.charAt(i)) {
+                    count++;;
                 }
             }
-            System.out.println(mostFrequent);
+            if (count>max) {
+                max=count;
+                mostFrequent=str.charAt(i);
+            }
+            else if (count==max) {
+                if(str.charAt(i)>=Integer.parseInt(Character.toString(mostFrequent))) {
+                    mostFrequent=str.charAt(i);
+                }
+            }
         }
+        System.out.println(mostFrequent);
     }
+}
